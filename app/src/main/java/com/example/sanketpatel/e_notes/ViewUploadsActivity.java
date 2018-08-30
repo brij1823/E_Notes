5508439d77f131b4e23ab97a08a0b4e186534fea
Brij S Patel <brijpatel1823@gmail.com>
2018-08-30 19:08:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sanketpatel/e_notes/ViewUploadsActivity.java
@@ -0,0 +1,111 @@
+package com.example.sanketpatel.e_notes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewUploadsActivity extends AppCompatActivity {
+    //the listview
+    ListView listView;
+
+    //database reference to get uploads data
+    DatabaseReference mDatabaseReference;
+
+    //list to store uploads data
+    List<Upload> uploadList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_uploads);
+
+
+        uploadList = new ArrayList<>();
+        listView = (ListView) findViewById(R.id.listView);
+
+
+        //adding a clicklistener on listview
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //getting the upload
+               Upload upload = uploadList.get(i);
+//
+//                //Opening the upload file in browser using the upload url
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse(upload.getUrl()));
+//                startActivity(intent);
+//                FirebaseStorage mFirebaseStorage = null;
+//                StorageReference photoRef = mFirebaseStorage.getReferenceFromUrl(upload.getUrl());
+//
+                Toast.makeText(ViewUploadsActivity.this,""+upload.getUrl(),Toast.LENGTH_SHORT).show();
+
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+                Query applesQuery = ref.child("uploads").orderByChild("url").equalTo(upload.getUrl());
+
+                applesQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot appleSnapshot: dataSnapshot.getChildren()) {
+                            appleSnapshot.getRef().removeValue();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                       // Log.e(TAG, "onCancelled", databaseError.toException());
+                    }
+                });
+
+
+
+            }
+        });
+
+
+        //getting the database reference
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference("uploads/");
+
+        //retrieving upload data from firebase database
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    Upload upload = postSnapshot.getValue(Upload.class);
+                    uploadList.add(upload);
+                }
+
+                String[] uploads = new String[uploadList.size()];
+
+                for (int i = 0; i < uploads.length; i++) {
+                    uploads[i] = uploadList.get(i).getName();
+                }
+
+                //displaying it to list
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(ViewUploadsActivity.this, android.R.layout.simple_list_item_1, uploads);
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+}
